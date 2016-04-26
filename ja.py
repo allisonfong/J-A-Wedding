f9d154b24e91ca08808f8b3b3a09c7e2af9be393
Allison Fong <allison.fong@globality.com>
2016-04-26 07:46:23 -0700

    Updates to RSVPModel, images, move to v2

--- ja.py
+++ ja.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import urllib
 
@@ -14,6 +15,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 DEFAULT_RSVP_NAME = 'default_rsvp'
 
+
 def rsvp_key(rsvp_name=DEFAULT_RSVP_NAME):
     """Constructs a Datastore key for a Guestbook entity.
 
@@ -21,12 +23,18 @@ def rsvp_key(rsvp_name=DEFAULT_RSVP_NAME):
     """
     return ndb.Key('RSVPModel', rsvp_name)
 
+
 class RSVPModel(ndb.Model):
     """Sub model for representing an author."""
-    rsvp = ndb.StringProperty(indexed=False)
-    name = ndb.StringProperty(indexed=False)
-    number = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
+    date = ndb.DateTimeProperty(auto_now=True)
+    email = ndb.StringProperty(indexed=False)
+    firstName = ndb.StringProperty(indexed=True)
+    group = ndb.IntegerProperty(indexed=True)
+    lastName = ndb.StringProperty(indexed=True)
+    message = ndb.StringProperty(indexed=False)
+    rsvp = ndb.IntegerProperty(indexed=True)
+    transportation = ndb.IntegerProperty(indexed=True)
+
 
 class MainPage(webapp2.RequestHandler):
 
@@ -41,39 +49,126 @@ class Invite(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('invite.html')
         self.response.write(template.render())
 
-class RSVP(webapp2.RequestHandler):
 
+class RSVPAdd(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('rsvp.html')
+        template = JINJA_ENVIRONMENT.get_template('rsvp_add.html')
         self.response.write(template.render())
 
     def post(self):
-        rsvp_name = self.request.get('rsvp_name',
-                                          DEFAULT_RSVP_NAME)
-        rsvpModel = RSVPModel(parent=rsvp_key(rsvp_name))
+        rsvp_first_name = self.request.get('first_name').lower()
+        rsvp_last_name = self.request.get('last_name').lower()
+        rsvp_group = int(self.request.get('group'))
 
-        rsvpModel.rsvp = self.request.get('rsvp')
-        rsvpModel.name = self.request.get('name')
-        rsvpModel.number = self.request.get('number')
-        rsvpModel.put()
+        rsvp = RSVPModel()
+        rsvp.firstName = rsvp_first_name
+        rsvp.lastName = rsvp_last_name
+        rsvp.group = rsvp_group
 
-        query_params = {'rsvp': rsvpModel.rsvp}
+        rsvp.put()
+
+        template = JINJA_ENVIRONMENT.get_template('rsvp_add.html')
+        self.response.write(template.render())
+
+
+class RSVPFind(webapp2.RequestHandler):
+
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('rsvp_find.html')
+        self.response.write(template.render())
+
+    def post(self):
+        rsvp_first_name = self.request.get('first_name').lower()
+        rsvp_last_name = self.request.get('last_name').lower()
+
+        query = RSVPModel.query(
+            RSVPModel.firstName == rsvp_first_name,
+            RSVPModel.lastName == rsvp_last_name
+        )
+
+        result = query.get()
+
+        if result is None:
+            logging.error(
+                rsvp_first_name + ' ' + rsvp_last_name + ' was not found'
+            )
+            template_values = {
+                'error': 'NOT_FOUND',
+                'message': 'Name not found, please try again or try another name in your party'
+            }
+            template = JINJA_ENVIRONMENT.get_template('rsvp_find.html')
+            self.response.write(template.render(template_values))
+        else:
+            query_params = {'group': result.group}
+            self.redirect('/rsvp/found?' + urllib.urlencode(query_params))
+
+
+class RSVP(webapp2.RequestHandler):
+
+    def get(self):
+        rsvp_group = int(self.request.get('group'))
+
+        rsvp_query = RSVPModel.query(RSVPModel.group == rsvp_group)
+
+        rsvps = rsvp_query.fetch()
+
+        template_values = {
+            'rsvps': rsvps,
+            'group': rsvp_group
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('rsvp.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        rsvp_yes = 0
+
+        rsvp_group = int(self.request.get('group'))
+
+        rsvp_query = RSVPModel.query(RSVPModel.group == rsvp_group)
+
+        rsvps = rsvp_query.fetch()
+
+        for rsvp in rsvps:
+            rsvp.rsvp = int(self.request.get('rsvp_'+rsvp.firstName))
+            if rsvp.rsvp and rsvp_yes == 0:
+                rsvp_yes = 1
+            else:
+                rsvp_yes = 2
+
+            rsvp.email = self.request.get('email')
+            if self.request.get('transportation'):
+                rsvp.transportation = 1
+            else:
+                rsvp.transportation = 0
+            rsvp.message = self.request.get('message')
+            rsvp.put()
+
+        query_params = {'rsvp': rsvp_yes}
         self.redirect('/thanks?' + urllib.urlencode(query_params))
+
 
 class Guestlist(webapp2.RequestHandler):
     def get(self):
-        rsvp_name = self.request.get('rsvp_name',
-                                          DEFAULT_RSVP_NAME)
-        rsvp_query = RSVPModel.query(
-        ancestor=rsvp_key(rsvp_name)).order(-RSVPModel.date)
-        rsvps = rsvp_query.fetch(30)
+        rsvp_query = RSVPModel.query().order(RSVPModel.group)
+        rsvps = rsvp_query.fetch(200)
+
+        rsvp_query_yes = RSVPModel.query(RSVPModel.rsvp == 1)
+        rsvps_yes = rsvp_query_yes.fetch(200)
+
+        rsvp_query_no = RSVPModel.query(RSVPModel.rsvp == 0)
+        rsvps_no = rsvp_query_no.fetch(200)
 
         template_values = {
-            'rsvps': rsvps
+            'rsvps': rsvps,
+            'total': len(rsvps),
+            'yes': len(rsvps_yes),
+            'no': len(rsvps_no)
         }
 
         template = JINJA_ENVIRONMENT.get_template('guestlist.html')
         self.response.write(template.render(template_values))
+
 
 class Thanks(webapp2.RequestHandler):
     def get(self):
@@ -86,10 +181,13 @@ class Thanks(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('thanks.html')
         self.response.write(template.render(template_values))
 
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/invite', Invite),
-    ('/rsvp', RSVP),
+    ('/rsvp', RSVPFind),
+    ('/rsvp/found', RSVP),
     ('/rsvp/guestlist', Guestlist),
+    ('/rsvp/add', RSVPAdd),
     ('/thanks', Thanks),
 ], debug=True)
